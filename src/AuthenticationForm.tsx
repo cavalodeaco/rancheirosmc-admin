fc14162c3cae0c02478bdd637e6d1043b641d8f6
Gian Maurício Fritsche <gianmauriciofritsche@MacBook-Air-de-Gian.local>
2022-09-30 07:40:35 -0300

    adequações de texto

--- src/AuthenticationForm.tsx
+++ src/AuthenticationForm.tsx
@@ -20,32 +20,32 @@ export function AuthenticationForm(props: PaperProps) {
 
     validate: {
       email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Informe seu endereço de e-mail'),
-      password: (val) => (val.length <= 6 ? 'Sua senha possui pelo menos 6 caracteres' : null),
+      password: (val) => (val.length <= 6 ? 'Sua chave de acesso possui pelo menos 6 caracteres' : null),
     },
   });
 
   return (
     <Paper radius="md" p="xl" withBorder {...props}>
       <Text size="lg" weight={500}>
-       Boas vindas ao sistema interno do PPV
+       Boas vindas ao sistema interno do projeto Pilotando Para Vida
       </Text>
 
-      <Divider label="Entre com seus dados" labelPosition="center" my="lg" />
+      <Divider label="Entre com seus dados de acesso" labelPosition="center" my="lg" />
 
       <form onSubmit={form.onSubmit(() => {})}>
         <Stack>
 
           <TextInput
             label="E-mail"
-            placeholder="jackson.teller@gmail.dev"
+            placeholder="jackson.teller@gmail.com"
             value={form.values.email}
             onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
             error={form.errors.email}
           />
 
           <PasswordInput
-            label="Password"
-            placeholder="Your password"
+            label="Chave de acesso"
+            placeholder="******"
             value={form.values.password}
             onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
             error={form.errors.password}
@@ -54,7 +54,7 @@ export function AuthenticationForm(props: PaperProps) {
         </Stack>
 
         <Group position="apart" mt="xl">
-          <Button type="submit">Login</Button>
+          <Button type="submit">Entrar</Button>
         </Group>
       </form>
     </Paper>
