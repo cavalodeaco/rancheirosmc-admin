9a4ea3937ffe67ec11a7a5cfdd745f1ce7d98039
Gian Maurício Fritsche <gianmauriciofritsche@MacBook-Air-de-Gian.local>
2023-03-12 21:05:30 -0300

    feat: create class form style

--- src/Class/CreateForm/index.tsx
+++ src/Class/CreateForm/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, createStyles, Stepper, UnstyledButton } from "@mantine/core";
+import { Alert, Button, Group, UnstyledButton } from "@mantine/core";
 import { useForm, zodResolver } from "@mantine/form";
 import { useLocalStorage } from "@mantine/hooks";
 import { IconAlertCircle, IconCircleCheck } from "@tabler/icons";
@@ -7,53 +7,30 @@ import { z } from "zod";
 import Tokens from "../../AuthenticationForm/Tokens";
 import Create from "../Create";
 
-const useStyles = createStyles((mantineTheme) => ({
-  form: {
-    backgroundColor: mantineTheme.white,
-    padding: mantineTheme.spacing.xl,
-    borderRadius: mantineTheme.radius.md,
-    boxShadow: mantineTheme.shadows.lg,
-  },
-
-  input: {
-    backgroundColor: mantineTheme.white,
-    borderColor: mantineTheme.colors.gray[4],
-    color: mantineTheme.black,
-
-    "&::placeholder": {
-      color: mantineTheme.colors.gray[5],
-    },
-  },
-
-  inputLabel: {
-    color: mantineTheme.black,
-  },
-
-  control: {
-    backgroundColor: mantineTheme.colors[mantineTheme.primaryColor][6],
-  },
-}));
-
 const pageSchema = z.object({
   class: z.object({
     // validate location using regex to identify the pattern https://goo.gl/maps/XXXXXX
-    location: z.string().refine((value) => {
-      const regex = new RegExp(
-        "^(https:\\/\\/goo\\.gl\\/maps\\/)([a-zA-Z0-9]{6})$"
-      );
-      return regex.test(value);
-    }, { message: "Localização inválida" }),
-    date: z.string().refine((value) => {
-      const date = new Date(value);
-      return date instanceof Date && !isNaN(date.getTime());
-    }, { message: "Data inválida" }),
-  city: z.enum(["curitiba", "maringá", "londrina", "cambira"]),
+    location: z.string().refine(
+      (value) => {
+        const regex = new RegExp(
+          "^(https:\\/\\/goo\\.gl\\/maps\\/)([a-zA-Z0-9]{6})$"
+        );
+        return regex.test(value);
+      },
+      { message: "Localização inválida" }
+    ),
+    date: z.string().refine(
+      (value) => {
+        const date = new Date(value);
+        return date instanceof Date && !isNaN(date.getTime());
+      },
+      { message: "Data inválida" }
+    ),
+    city: z.enum(["curitiba", "maringá", "londrina", "cambira"]),
   }),
 });
 
-
 export function CreateForm() {
-  const { classes } = useStyles();
   const [tokens, setTokens] = useLocalStorage<Tokens>({
     key: "tokens",
   });
@@ -66,24 +43,22 @@ export function CreateForm() {
       class: {
         city: "curitiba",
         location: "",
-        date: ""
-      }
+        date: "",
+      },
     },
   });
 
   const submitForm = async (): Promise<void> => {
     if (!page.validate().hasErrors) {
       setResult(0);
-      const data = JSON.stringify(
-        page.values["class"]
-      );
+      const data = JSON.stringify(page.values["class"]);
       const config = {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          // add tokens from localstorage   
-          "access_token": `${tokens.access_token}`,
-          "id_token": `${tokens.id_token}`
+          // add tokens from localstorage
+          access_token: `${tokens.access_token}`,
+          id_token: `${tokens.id_token}`,
         },
         body: data,
       };
@@ -107,22 +82,28 @@ export function CreateForm() {
 
   return (
     <div style={{ position: "relative" }}>
-      <Create page={page} useStyles={useStyles} />
-      <Button onClick={submitForm}>Enviar</Button>
-      {[
-        <UnstyledButton />,
-        <Alert
-          icon={<IconAlertCircle size={16} />}
-          title="Não conseguimos fazer o cadastro da turma"
-          color="red.6"
-        >
-          Entre em contato.
-        </Alert>,
-        <Alert
-          icon={<IconCircleCheck size={16} />}
-          title="Turma cadastrada!"
-          color="teal.6" children={undefined} />
-      ][result]}
-    </div >
-  )
+      <Create page={page} />
+      <Group position="apart" mt="xl">
+        <Button onClick={submitForm}>Enviar</Button>
+      </Group>
+      {
+        [
+          <UnstyledButton />,
+          <Alert
+            icon={<IconAlertCircle size={16} />}
+            title="Não conseguimos fazer o cadastro da turma"
+            color="red.6"
+          >
+            Entre em contato.
+          </Alert>,
+          <Alert
+            icon={<IconCircleCheck size={16} />}
+            title="Turma cadastrada!"
+            color="teal.6"
+            children={undefined}
+          />,
+        ][result]
+      }
+    </div>
+  );
 }
